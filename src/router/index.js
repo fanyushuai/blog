f4520e843ae7638a01d2733f82dfc3c821ce37b2
fanyushuai <272253157@qq.com>
2020-08-12 21:48:07 +0800

    。。。

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import list from '@/admin/list.vue'
 import Main from '@/components/Main'
 import Contract from '@/components/system/Contract'
@@ -11,22 +10,21 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },{
-      path: '/list',
-      name: 'list',
-      component: list
-    },{
-      path: '/system',
       name: '系统中心',
       component: Main,
       iconCls: 'fa fa-address-card',
-      children: [{
+      children: [
+        {
           path: '/system/contract',
           component: Contract,
           name: '联系人管理'
-      }]
+        },
+        {
+          path: '/list',
+          name: '文章管理',
+          component: list
+        },
+      ]
     }
   ]
 })
